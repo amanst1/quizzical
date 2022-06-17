fe5cd0de4e33dd567f3ceb96d3b8d2204e791147
AmanS <seifeamanuel@gmail.com>
2022-06-17 12:25:46 +0300

    finished game start flow

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import Question from './components/Question';
 import Welcome from './components/Welcome';
 import './styles.css';
 
 export default function App() {
+  const [start, setStart] = React.useState(true)
+
+  function beginGame () {
+    setStart(false)
+  }
+
   return (
       <div className='container'> 
-        <Welcome/>
+        {start ? <Welcome start={beginGame} /> : <Question/>}
+        
       </div>
   );
 }
--- quizzical/src/components/Welcome.js
+++ quizzical/src/components/Welcome.js
@@ -1,12 +1,16 @@
 import React from "react"
 
-export default function Welcome () {
+export default function Welcome (props) {
+    //console.log(props)
+   
+
     return (
         <div>
+
             <h1>QUIZZICAL</h1>
             <p>The game to test your know hows</p>
 
-            <button> Start quiz</button>
+            <button onClick={()=>props.start()} > Start quiz</button>
         </div>
     )
 
