bac981532c68436d60ac980a22c8ef14409a6f35
AmanS <seifeamanuel@gmail.com>
2022-06-19 23:41:41 +0300

    checked for correct answers using state inside question js, hold the position of choices from changing using sideeffect

--- quizzical/src/components/Question.js
+++ quizzical/src/components/Question.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 export default function Question(props) {
+
+    const[correct, setCorrect] = React.useState(false)
+    const[randomChoices, setRandomChoices] = React.useState([])
     
     function shuffleArray(arr) {
         for (let i = arr.length - 1; i > 0; i--) {
@@ -11,16 +14,31 @@ export default function Question(props) {
         }
         return arr
     }
+
+    
+    function checkAnswer(value) {
+        return props.correct_answer === value ? setCorrect(true) : setCorrect(false)
+    
+    }
      
-    const choices = shuffleArray([...props.incorrect_answers, props.correct_answer])
+    
+    // load and store randomized answers in state
+    React.useEffect(()=> {
+        const choices = shuffleArray([...props.incorrect_answers, props.correct_answer])
 
-    const randomChoices = choices.map( (choice,index) => <button key={index}>{choice}</button> )
+        setRandomChoices(() => {
+            const randomized = choices.map( (choice,index) => <button onClick={() => checkAnswer(choice)} key={index}>{choice}</button> )
+            return randomized
+        })
+    }, [])
+    
+    //console.log(props.correct_answer)
 
-    console.log(props.correct_answer)
     return (
         <div>
             <p>{props.question}</p>
             {randomChoices}
+            {correct && <h3> "Correct Answer"</h3>} 
             <hr/>
             
         </div>
