3779057e8405dc9a828671dcd92305dcc11be037
AmanS <seifeamanuel@gmail.com>
2022-06-21 01:23:42 +0300

    added state for correct answer count and object holding user selected answers, add functions to count correct answers and receive data from child component and organize user input

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -6,8 +6,9 @@ import './styles.css';
 export default function App() {
   const [start, setStart] = React.useState(true)
   const [quizData, setQuizData] = React.useState([]) //remember lazy loading
+  const [selectedChoices, setSelectedChoices] = React.useState({})
+  const [correctCount, setCorrectCount] = React.useState(0) 
 
-  
   //fetch questions from api
   React.useEffect(()=>{
     fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple")
@@ -20,8 +21,36 @@ export default function App() {
     setStart(false)
   }
   
- 
-  const questions = quizData.map( (quiz, index) => (<Question key={index} {...quiz} />) )
+  //adds user's selected answer for each question to a single selectedChoices object
+  function addSelectedChoice(index, isCorrect) {
+    setSelectedChoices(prevSelected => {
+      return {
+              ...prevSelected, 
+              [index] : isCorrect 
+            }
+          })
+            
+        
+  }
+  // console.log(selectedChoices, "object")
+  // console.log(correctCount, "count")
+
+  // counts the correct answers selected by user
+  function countCorrect() {
+      if(quizData.length === Object.keys(selectedChoices).length) {
+        let count = 0
+        for(const selected in selectedChoices) {
+          console.log(selectedChoices.selected)
+          selected && count++ //work on object attr names
+        }
+        setCorrectCount(count)
+       }
+       
+  }
+
+  //populates each question in to a Question js components array
+  const questions = quizData.map( (quiz, index) => 
+                (<Question recordChoices={addSelectedChoice} key={index} qnum={index} {...quiz} />) )
 
   return (
       <div className='container'> 
@@ -29,7 +58,7 @@ export default function App() {
           <Welcome start={beginGame} /> :
            questions
         }
-        {!start && <button> Check Answer</button> }
+        {!start && <button onClick={countCorrect}> Check Answer</button> }
       </div>
   );
 }
